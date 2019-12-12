a720d8b4ecd2a9a2e244586382c8c53b14376c7e
James <jinglis12@googlemail.com>
2019-12-12 17:11:14 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        ProjectBacker.all.map { |e| 
+            if e.backer.name == self.name
+            e.project
+            end
+        }.compact
+    end
+
+end
